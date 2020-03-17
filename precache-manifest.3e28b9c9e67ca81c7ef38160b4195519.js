bb1a45abc2277d67df844fda09e9584fb718ecd3
AlaminAB <alaminislam907@gmail.com>
2020-03-17 15:29:18 +0600

    upload

---
+++ precache-manifest.3e28b9c9e67ca81c7ef38160b4195519.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d92cff3925775d1ab0fbfd7807b8fcff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b13015cd1922495cb458",
+    "url": "/static/css/main.60e94aef.chunk.css"
+  },
+  {
+    "revision": "635b7bc9ac7fc29a8599",
+    "url": "/static/js/2.ef2c6f2f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.ef2c6f2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b13015cd1922495cb458",
+    "url": "/static/js/main.ba8a95f8.chunk.js"
+  },
+  {
+    "revision": "c2f98a5c387da539a513",
+    "url": "/static/js/runtime-main.5e682c5b.js"
+  }
+]);
